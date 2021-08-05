e0d7e7d95ed44e2e9a1fe6b35b4532d75696c7b5
Rebecca <rebeccadumazert@hotmail.fr>
2021-08-05 09:58:36 +0200

    Books

---
+++ src/components/Books/Books.js
@@ -0,0 +1,26 @@
+import './Books.css'
+
+import React, { useEffect, useState } from 'react'
+
+import Book from './components/Book/Book'
+import { getBooks } from '../../services/api'
+
+function Books() {
+  const [books, setBooks] = useState([])
+  useEffect(() => {
+    const getBooksDatas = async () => {
+      const datas = await getBooks()
+      setBooks(datas)
+    }
+    getBooksDatas()
+  }, [])
+  return (
+    <div className='Books'>
+      {books.map((book, i) => (
+        <Book key={i} bookdata={book}></Book>
+      ))}
+    </div>
+  )
+}
+
+export default Books
