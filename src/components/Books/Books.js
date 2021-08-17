2254a21b4f55bccf1fa28a25e6ded57aa0d8c222
Rebecca <rebeccadumazert@hotmail.fr>
2021-08-18 00:05:38 +0200

    books

--- src/components/Books/Books.js
+++ src/components/Books/Books.js
@@ -4,9 +4,12 @@ import React, { useEffect, useState } from 'react'
 
 import Book from './components/Book/Book'
 import { getBooks } from '../../services/api'
+import { useSelector } from 'react-redux'
 
 function Books() {
   const [books, setBooks] = useState([])
+  const resultsSearch = useSelector((state) => state.resultsSearch.current)
+
   useEffect(() => {
     const getBooksDatas = async () => {
       const datas = await getBooks()
@@ -14,11 +17,22 @@ function Books() {
     }
     getBooksDatas()
   }, [])
+
   return (
-    <div className='Books'>
-      {books.map((book, i) => (
-        <Book key={i} book={book}></Book>
-      ))}
+    <div>
+      {resultsSearch.length === 0 ? (
+        <div className='Books'>
+          {books.map((book, i) => (
+            <Book key={i} book={book}></Book>
+          ))}
+        </div>
+      ) : (
+        <div className='Books'>
+          {resultsSearch.map((book, i) => (
+            <Book key={i} book={book[i]}></Book>
+          ))}
+        </div>
+      )}
     </div>
   )
 }
