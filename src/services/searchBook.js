9c6e5a49202da5d05be4d5bd8aad00904a19615c
Rebecca <rebeccadumazert@hotmail.fr>
2021-08-18 00:06:39 +0200

    clean app

--- src/services/searchBook.js
+++
@@ -1,6 +0,0 @@
-export const findResultBySearch = (search, booksList) => {
-  console.log(booksList.length, 'hey')
-  const searchRegex = new RegExp(`/*${search}*`)
-  const resultSearch = booksList.filter((book) => searchRegex.test(book.title))
-  return resultSearch.map((result) => result.title)
-}
