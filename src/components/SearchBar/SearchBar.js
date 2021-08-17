77d19712cb61ec4d88a6ae127a096f52df111a75
Rebecca <rebeccadumazert@hotmail.fr>
2021-08-18 00:06:27 +0200

    searchBar

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -4,15 +4,26 @@ import React, { useEffect, useState } from 'react'
 
 import FlashOnIcon from '@material-ui/icons/FlashOn'
 import { Link } from 'react-router-dom'
-import { Results } from './components/Results'
 import ShoppingCartIcon from '@material-ui/icons/ShoppingCart'
-import { findResultBySearch } from '../../services/searchBook'
+import { booksBySearch } from '../../store/resultSearch'
 import { getBooks } from '../../services/api'
+import { useDispatch } from 'react-redux'
+import { useSelector } from 'react-redux'
 
 export const SearchBar = () => {
+  const dispatch = useDispatch()
   const [search, setSearch] = useState([''])
   const [books, setBooks] = useState([])
   const [titleFromSearch, setTitleFromSearch] = useState([])
+  const totalItemsInCart = useSelector((state) => state.cart.current).length
+
+  function findResultBySearch(search, booksList) {
+    const searchRegex = new RegExp(`/*${search}*`, 'i')
+    const resultSearch = booksList.filter((book) =>
+      searchRegex.test(book.title)
+    )
+    dispatch(booksBySearch(resultSearch))
+  }
 
   useEffect(() => {
     const getBooksDatas = async () => {
@@ -27,34 +38,44 @@ export const SearchBar = () => {
     setTitleFromSearch(titles)
     setSearch('')
   }
-
   const getSearchByCustomer = ({ target: { value } }) => {
     setSearch(value)
   }
   return (
     <div>
-      <nav className='SearchBar'>
-        <Link to='/'>
+      <nav className='Head'>
+        <Link to='/' className='Icon'>
           <FlashOnIcon style={{ margin: '5px' }}></FlashOnIcon>
         </Link>
-        <input
-          type='text'
-          placeholder='Trouve livre à ton pied'
-          value={search}
-          onChange={getSearchByCustomer}
-        />
-        <button onClick={getTitle}>
-          <img
-            src='https://img.icons8.com/material-outlined/24/000000/search--v1.png'
-            alt=''
+        <div className='SearchBar'>
+          <input
+            type='text'
+            placeholder='Trouve livre à ton pied'
+            value={search}
+            onChange={getSearchByCustomer}
           />
-        </button>
+          {search === '' ? (
+            <span></span>
+          ) : (
+            <Link to='/search'>
+              <img
+                onClick={getTitle}
+                src='https://img.icons8.com/material-outlined/24/000000/search--v1.png'
+                alt=''
+              />
+            </Link>
+          )}
+        </div>
 
-        <Link to='/cart'>
-          <ShoppingCartIcon style={{ margin: '5px' }}></ShoppingCartIcon>
+        <Link
+          to='/cart'
+          className='Icon'
+          style={{ display: 'flex', alignItems: 'center' }}
+        >
+          <ShoppingCartIcon></ShoppingCartIcon>
+          <p>{totalItemsInCart}</p>
         </Link>
       </nav>
-      <Results results={titleFromSearch}></Results>
     </div>
   )
 }
